ab813c5759069d777a9fc7832e4013cfcea550e9
CalebeLAR <ca6ele@gmail.com>
2022-11-03 15:35:43 -0400

    req 1: Testes do componente App teste 2

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,37 +1,59 @@
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
 describe('Testes do componente <App.js />', () => {
-  test('(1) a aplicação deve conter um topo com um conjunto de links de navegação.', () => {
-    renderWithRouter(<App />);
+  describe('Barra de navegação', () => {
+    test('(1) a aplicação deve conter um topo com um conjunto de links de navegação.', () => {
+      renderWithRouter(<App />);
 
-    // expects para o topo "barra de navegação"
-    const nav = screen.getByRole('navigation');
-    expect(nav).toBeVisible();
+      // expects para o topo "barra de navegação"
+      const nav = screen.getByRole('navigation');
+      expect(nav).toBeVisible();
 
-    // expects do link "home"
-    const linkHome = screen.getByRole('link', { name: 'Home' });
-    expect(linkHome).toHaveAttribute('class', 'link');
-    expect(linkHome).toHaveAttribute('href', '/');
-    expect(linkHome).toBeVisible();
+      // expects do link "home"
+      const linkHome = screen.getByRole('link', { name: 'Home' });
+      expect(linkHome).toHaveAttribute('class', 'link');
+      expect(linkHome).toHaveAttribute('href', '/');
+      expect(linkHome).toBeVisible();
 
-    // expects do link "home"
-    const linkAbout = screen.getByRole('link', { name: 'About' });
-    expect(linkAbout).toHaveAttribute('class', 'link');
-    expect(linkAbout).toHaveAttribute('href', '/about');
-    expect(linkAbout).toBeVisible();
+      // expects do link "home"
+      const linkAbout = screen.getByRole('link', { name: 'About' });
+      expect(linkAbout).toHaveAttribute('class', 'link');
+      expect(linkAbout).toHaveAttribute('href', '/about');
+      expect(linkAbout).toBeVisible();
 
-    // expects do link "home"
-    const linkFavoritePokémons = screen.getByRole('link', { name: 'Favorite Pokémons' });
-    expect(linkFavoritePokémons).toHaveAttribute('class', 'link');
-    expect(linkFavoritePokémons).toHaveAttribute('href', '/favorites');
-    expect(linkFavoritePokémons).toBeVisible();
+      // expects do link "home"
+      const linkFavoritePokémons = screen.getByRole('link', { name: 'Favorite Pokémons' });
+      expect(linkFavoritePokémons).toHaveAttribute('class', 'link');
+      expect(linkFavoritePokémons).toHaveAttribute('href', '/favorites');
+      expect(linkFavoritePokémons).toBeVisible();
+    });
   });
+  describe('Click nos links de navegação', () => {
+    test('(2) ao clicar no link "Home" a plicação deve redirecionar para a página principal "/".', () => {
+      const { history } = renderWithRouter(<App />);
 
-  test.todo('(2) ao clicar no link "Home" a plicação deve redirecionar para a página principal "/".');
-  test.todo('(3) ao clicar no link "About" a plicação deve redirecionar para a página principal "/about".');
-  test.todo('(4) ao clicar no link "Favorite Pokémons" a plicação deve redirecionar para a página de pokemons favoritados "/favorites".');
-  test.todo('(5) a aplicação redireciona para a página notFound caso a URL seja desconhecida.');
+      // navegando da página about para a home
+      history.push('/about');
+      userEvent.click(screen.getByRole('link', { name: 'Home' }));
+      expect(history.location.pathname).toBe('/');
+
+      // navegando da página favorite pokemon para a home
+      history.push('/favorites');
+      userEvent.click(screen.getByRole('link', { name: 'Home' }));
+      expect(history.location.pathname).toBe('/');
+
+      // navegando da página NotFound pokemon para a home
+      history.push('/PaginaNotFund');
+      userEvent.click(screen.getByRole('link', { name: 'Home' }));
+      expect(history.location.pathname).toBe('/');
+    });
+    test.todo('(3) ao clicar no link "About" a plicação deve redirecionar para a página principal "/about".');
+    test.todo('(4) ao clicar no link "Favorite Pokémons" a plicação deve redirecionar para a página de pokemons favoritados "/favorites".');
+  });
+  describe('Not Found', () => {
+    test.todo('(5) a aplicação redireciona para a página notFound caso a URL seja desconhecida.');
+  });
 });
