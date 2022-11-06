8c0376868c45092a5d18c90aef6c27555e07e38e
CalebeLAR <ca6ele@gmail.com>
2022-11-06 15:49:36 -0400

    req 4: dois testes

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('#NotFound.', () => {
+  beforeEach(() => {
+    renderWithRouter(<NotFound />);
+  });
+
+  test('1) O componente deve conter um heading h2 com o texto Page requested not found', () => {
+    const mainTitle = screen.getByRole('heading', { name: 'Page requested not found' });
+    expect(mainTitle).toBeVisible();
+  });
+
+  test('2)  O componente deve conter a imagem do Pikachu chorando.', () => {
+    const imgSadPikachu = screen.getByRole('img');
+    expect(imgSadPikachu).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+    expect(imgSadPikachu).toHaveAttribute('alt', 'Pikachu crying because the page requested was not found');
+    expect(imgSadPikachu).toBeVisible();
+  });
+});
