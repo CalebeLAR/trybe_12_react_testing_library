59078ce7e474c6fce08f71158e74773fb22f4592
CalebeLAR <ca6ele@gmail.com>
2022-11-06 16:39:04 -0400

    req 5: describe 1, teste 1

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,24 +1,33 @@
-// import { screen } from '@testing-library/react';
-// import renderWithRouter from '../renderWithRouter';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
 // import userEvent from '@testing-library/user-event';
+import App from '../App';
 
 describe('#Pokedex', () => {
-  test.todo('Teste se a página contém um heading h2 com o texto Encountered pokémons');
-  describe('Testes para a exibição dos pokémons da lista quando o botão "Próximo pokémon" é clicado', () => {
-    test.todo('o botão deve conter o texto "Próximo pokémon"');
-    test.todo('os próximos pokémons da lista devem ser mostrados, um a um, ao clicar sucessivamente no botão;');
-    test.todo('o primeiro pokémon da lista deve ser mostrado ao clicar no botão, se estiver no último pokémon da lista.');
-    test.todo('deve ser mostrado apenas um pokémon por vez');
+  beforeEach(() => {
+    renderWithRouter(<App />);
   });
-  describe('Teste se a Pokédex tem os botões de filtro', () => {
-    test.todo('Deve existir um botão de filtragem para cada tipo de pokémon, sem repetição');
-    test.todo('A partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo.');
-    test.todo('O texto do botão deve corresponder ao nome do tipo, ex. Psychic.');
-    test.todo('O botão All precisa estar sempre visível.');
+  describe('Testes para a janela de exibição dos pokemóns.', () => {
+    test('a página deve conter um heading h2 com o texto Encountered pokémons', () => {
+      const headingPokedex = screen.getByRole('heading', { name: 'Encountered pokémons', level: 2 });
+      expect(headingPokedex).toBeVisible();
+    });
+  });
+  describe('Testes para a exibição dos pokémons da lista quando o botão "Próximo pokémon" é clicado', () => {
+    test.todo('o botão deve conter o texto "Próximo pokémon".');
+    test.todo('os próximos pokémons da lista devem ser mostrados, um a um, ao clicar sucessivamente no botão.');
+    test.todo('o primeiro pokémon da lista deve ser mostrado ao clicar no botão, se estiver no último pokémon da lista.');
+    test.todo('deve ser mostrado apenas um pokémon por vez.');
+  });
+  describe('Testes para os botões que filtram os pokemóns da Pokédex pelo tipo.', () => {
+    test.todo('deve existir um botão de filtragem para cada tipo de pokémon, sem repetição');
+    test.todo('a partir da seleção de um botão de tipo, a Pokédex deve circular somente pelos pokémons daquele tipo.');
+    test.todo('o texto do botão deve corresponder ao nome do tipo, ex. Psychic.');
+    test.todo('o botão All precisa estar sempre visível.');
   });
   describe('Teste se a Pokédex contém um botão para resetar o filtro', () => {
-    test.todo('O texto do botão deve ser All;');
-    test.todo(' Pokedéx deverá mostrar os pokémons normalmente (sem filtros) quando o botão All for clicado;');
-    test.todo('Ao carregar a página, o filtro selecionado deverá ser All');
+    test.todo('o texto do botão deve ser All.');
+    test.todo('a Pokedéx deverá mostrar os pokémons normalmente (sem filtros) quando o botão All for clicado.');
+    test.todo('ao carregar a página, o filtro selecionado deverá ser All.');
   });
 });
