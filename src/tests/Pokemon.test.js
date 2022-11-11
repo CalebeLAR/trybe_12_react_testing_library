b327296d66f07d2f28f667eaf864bc9980b3c4e2
CalebeLAR <ca6ele@gmail.com>
2022-11-11 11:04:42 -0400

    feat: req 6) describe 1, teste 2

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -9,15 +9,22 @@ describe('#Pokemon', () => {
   // });
   describe('Testes referentes ao card com as informações do pokémon', () => {
     test('(1) o link de detalhes do pokemon deve redirecionamentar para a página de detalhes de pokémon', async () => {
+      renderWithRouter(<App />);
+      const moreDetails = screen.getByText('More details');
+      const pikachu = screen.getByText('Pikachu');
+      expect(moreDetails).toHaveAttribute('href', '/pokemons/25');
+      expect(moreDetails).toBeVisible();
+    });
+    test('(2) a página de detalhes deve ter um URL com a configuração /pokemon/<id do pokemon>', async () => {
       const { history } = renderWithRouter(<App />);
       const moreDetails = screen.getByText('More details');
       const pikachu = screen.getByText('Pikachu');
+
       userEvent.click(moreDetails);
 
-      const pikachuDetails = await screen.findByText('Pikachu Details');
+      await screen.findByText('Pikachu Details');
       expect(history.location.pathname).toBe('/pokemons/25');
     });
-    test.todo('(2) a página de detalhes deve ter um URL com a configuração /pokemon/<id do pokemon>');
   });
   describe('Testes referentes aos pokemons favoritados', () => {
     test.todo('(3) o ícone deve ser uma imagem com o atributo src contendo o caminho /star-icon.svg');
